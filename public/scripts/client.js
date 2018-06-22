e5cbf0af3f910c0837fcbe638264ae7c81510e13
Greg.Wallerus <gwallerus@gmail.com>
2018-06-22 11:58:09 -0500

    hide show

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,53 @@
+hideQuestions()
+
+function hideQuestions() {
+    $('#question2').hide()
+    $('#question3').hide()
+    $('#question4').hide()
+}
+
+$('#question1Button').click(function(){
+    $('#question1').hide()
+    $('#question2').show()
+    console.log('question 1 answered')
+})
+
+$('#question2Button').click(function () {
+    $('#question2').hide()
+    $('#question3').show()
+    console.log('question 2 answered')
+})
+
+$('#question3Button').click(function () {
+    $('#question3').hide()
+    $('#question4').show()
+    console.log('question 3 answered')
+})
+
+$('#question3PreviousButton').click(function () {
+    $('#question3').hide()
+    $('#question4').show()
+    console.log('question 3 answered')
+})
+
+$('#question2PreviousButton').click(function () {
+    $('#question3').hide()
+    $('#question4').show()
+    console.log('question 3 answered')
+})
+
+$('#question1PreviousButton').click(function () {
+    $('#question3').hide()
+    $('#question4').show()
+    console.log('question 3 answered')
+})
+
 $('#answers').click(function () {
 
-    let question1 = "Horses Horses Horses"
-    let question2 = "Dogs Dogs Dogs"
-    let question3 = "Cats Cats Cats"
-    let question4 = "More Animals Please"
+    let question1 = $('#questionOne').val()
+    let question2 = $('#questionTwo').val()
+    let question3 = $('#questionThree').val()
+    let question4 = $('#questionFour').val()
 
     $.ajax({
         type: 'POST',
